72960e6c19fb5afbf9088c7550a7bf3ebd411bd1
SkrillK <90263562+SkrillK@users.noreply.github.com>
2024-03-15 01:16:18 +0300

    v2.0
    works right

--- FordFalkerson.cpp
+++ FordFalkerson.cpp
@@ -1,81 +1,73 @@
 ﻿#include <iostream>
 #include <vector>
-#include <cstdio>
 #include <queue>
-#include <limits>
+#include <fstream>
 
 using namespace std;
 
 struct Edge 
 {
-    int vertex, capacity, flow;
-
-    Edge(int vertex, int capacity) : vertex(vertex), capacity(capacity), flow(0) {}
+    int vertex;
+    int flow;
+    int capacity;
 };
-
-bool breadthFirstSearch(vector<vector<Edge>>& newGraph, vector<int>& parent, int node, int drainage) 
-{
-    vector<bool> visited(newGraph.size(), false);
-    queue<int> vertexQueue;
-    int u, vertex, capacity, flow;
-
-    vertexQueue.push(node);
-    visited[node] = true;
-    parent[node] = -1;
-
-    while (!vertexQueue.empty()) {
-        u = vertexQueue.front();
-        vertexQueue.pop();
-
-        for (const auto& edge : newGraph[u]) {
-            vertex = edge.vertex;
-            capacity = edge.capacity;
-            flow = edge.flow;
-
-            if (capacity - flow > 0 && !visited[vertex]) {
-                vertexQueue.push(vertex);
-                parent[vertex] = u;
-                visited[vertex] = true;
-            }
-        }
-    }
-
-    return visited[drainage];
-}
 
 int maxFlowFind(vector<vector<Edge>>& graph, int source, int drainage) 
 {
     vector<vector<Edge>> newGraph(graph);
-    vector<int> parent(graph.size());
     int maxFlow = 0, pathFlow, u;
 
-    while (breadthFirstSearch(newGraph, parent, source, drainage)) 
-    {
-        pathFlow = numeric_limits<int>::max();
 
-        for (int vertex = drainage; vertex != source; vertex = parent[vertex]) 
+    while (true)
+    {
+        vector<int> parent(graph.size(), -1);
+        queue<int> q;
+        q.push(source);
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (Edge& edge : newGraph[u])
+                if (parent[edge.vertex] == -1 && edge.capacity > 0) {
+                    q.push(edge.vertex);
+                    parent[edge.vertex] = u;
+                }
+        }
+
+        if (parent[drainage] == -1) {
+            break;
+        }
+
+        pathFlow = INT_MAX;
+        for (int vertex = drainage; vertex != source; vertex = parent[vertex])
         {
             u = parent[vertex];
-            for (auto& edge : newGraph[u]) 
+            for (Edge& edge : newGraph[u])
                 if (edge.vertex == vertex) {
-                    pathFlow = min(pathFlow, edge.capacity - edge.flow);
-                    edge.flow += pathFlow;
+                    pathFlow = min(pathFlow, edge.capacity);
                     break;
                 }
         }
 
         maxFlow += pathFlow;
+
+        for (int v = drainage; v != source; v = parent[v]) {
+            int u = parent[v];
+            for (Edge& edge : newGraph[u])
+                if (edge.vertex == v) {
+                    edge.capacity -= pathFlow;
+                    for (Edge& revEdge : newGraph[v])
+                        if (revEdge.vertex == u) {
+                            revEdge.capacity += pathFlow;
+                            break;
+                        }
+                    break;
+                }
+        }
     }
 
     return maxFlow;
-}
-
-void minCut(vector<vector<Edge>>& graph, int source, vector<bool>& visited) 
-{
-    visited[source] = true;
-    for (const auto& edge : graph[source])
-        if (!visited[edge.vertex] && edge.capacity - edge.flow > 0)
-            minCut(graph, edge.vertex, visited);
 }
 
 int main() 
@@ -84,42 +76,35 @@ int main()
     int source, drainage;
     int u, vertex, capacity;
     int maxFlow;
-    FILE* str;
+    ifstream file;
+    file.open("input_graph.txt");
 
     //------------------------------------------------------------
 
-    freopen_s(&str, "input_graph.txt", "r", stdin);
-    cin >> numOfVertexes >> numOfEdges;
-    cin >> source >> drainage;
+    file >> numOfVertexes >> numOfEdges;
+    file >> source >> drainage;
 
     cout << "Num of vertexes: " << numOfVertexes << "\n";
     cout << "Number of edges: " << numOfEdges << "\n";
-    cout << "Path: " << source << " -> " << drainage << "\n\n";
+    cout << "Path: " << source + 1 << " -> " << drainage + 1 << "\n\n";
 
     //------------------------------------------------------------
 
-    numOfVertexes++;
     vector<vector<Edge>> graph(numOfVertexes);
 
-    for (int i = 0; i < numOfEdges; ++i) {
-        cin >> u >> vertex >> capacity;
-        graph[u - 1].emplace_back(vertex, capacity);
+    cout << "Number\tEdge\tCapacity\n";
+    for (int i = 0; i < numOfEdges; i++) {
+        file >> u >> vertex >> capacity;
+        graph[u - 1].push_back({ vertex - 1, capacity, capacity });
 
-        cout << "Edge " << i + 1 << ": (" << u << ";" << vertex << ") = " << capacity << "\n";
+        cout << i + 1 << "\t(" << u << "," << vertex << ")\t" << capacity << "\n";
     }
 
     maxFlow = maxFlowFind(graph, source, drainage);
 
-    vector<bool> visited(numOfVertexes, false);
-    minCut(graph, source, visited);
-
     //------------------------------------------------------------
 
-    cout << "\nMax flow: " << maxFlow << endl;
-    cout << "Min cut: ";
-    for (int i = 0; i < numOfVertexes; ++i)
-        if (visited[i])
-            cout << i << " ";
+    cout << "\nMax flow: " << maxFlow << "\n";
 
     return 0;
 }
